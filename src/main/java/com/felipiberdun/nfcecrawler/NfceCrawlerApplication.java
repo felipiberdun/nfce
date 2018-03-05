5b16253510be3adbaa3e3df4431990816576a53a
Felipi Berdun <felipi.berdun@mercadolivre.com>
2018-03-05 01:55:06 -0300

    Create Spring boot application

---
+++ src/main/java/com/felipiberdun/nfcecrawler/NfceCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.felipiberdun.nfcecrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfceCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfceCrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipiberdun/nfcecrawler/NfceCrawlerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.felipiberdun.nfcecrawler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NfceCrawlerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
